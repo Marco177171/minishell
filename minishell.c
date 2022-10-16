4664ea8420e3c064cc8d610449363236e113e8d0
Marco Sebastiani <masebast@e4r2p3.42roma.it>
2022-10-16 18:30:26 +0200

    check all cases in redirect

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 17:12:49 by masebast          #+#    #+#             */
-/*   Updated: 2022/10/16 18:08:01 by masebast         ###   ########.fr       */
+/*   Updated: 2022/10/16 18:10:51 by masebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -213,7 +213,6 @@ void	ft_execute_cycle(t_command *command_struct, char **envp)
 			ft_remove_quotes(command_struct->word_matrix[0]);
 			if (ft_check_redirection(command_struct->word_matrix) == 1)
 			{
-				printf("redirection detected: defining custom output...\n");
 				ft_redirect(command_struct, 0, envp);
 				ft_free_matrix(command_struct->word_matrix);
 				ft_free_matrix(command_struct->pipe_matrix);
