6c977cec16bda4ab131bffff4474c13a3dbee117
Marco Sebastiani <masebast@e4r2p3.42roma.it>
2022-10-16 17:53:56 +0200

    output redirections created

--- ft_redirections.c
+++ ft_redirections.c
@@ -6,7 +6,7 @@
 /*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:45:04 by masebast          #+#    #+#             */
-/*   Updated: 2022/10/16 17:29:51 by masebast         ###   ########.fr       */
+/*   Updated: 2022/10/16 17:52:10 by masebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,15 @@ void	ft_redirect(t_command *command_struct, int pipe_index, char **envp)
 			ft_strncmp(command_struct->word_matrix[index], ">\0", 2) == 0)
 		{
 			if (ft_strncmp(command_struct->word_matrix[index], ">>\0", 3) == 0)
-				fd = open(command_struct->word_matrix[index + 1], O_APPEND|O_CREAT|O_WRONLY, 0777);
+				fd = open(command_struct->word_matrix[index + 1], O_APPEND|O_CREAT|O_WRONLY, 0644);
 			else if (ft_strncmp(command_struct->word_matrix[index], ">\0", 2) == 0)
-				fd = open(command_struct->word_matrix[index + 1], O_CREAT|O_WRONLY, 0777);
+				fd = open(command_struct->word_matrix[index + 1], O_TRUNC|O_CREAT|O_WRONLY, 0644);
 			close(STDOUT_FILENO);
 			dup2(fd, STDOUT_FILENO);
 			ft_recognize_command(command_struct, pipe_index, envp);
 			dup2(stdoutcpy, STDOUT_FILENO);
 			close(fd);
 		}
-		// else if (ft_strncmp(command_struct->word_matrix[index], ">\0", 2) == 0)
-		// {
-		// 	printf("> found\n");
-		// 	fd = open(command_struct->word_matrix[index + 1], O_CREAT|O_WRONLY, 0777);
-		// 	close(STDOUT_FILENO);
-		// 	dup2(fd, STDOUT_FILENO);
-		// 	ft_recognize_command(command_struct, pipe_index, envp);
-		// 	dup2(stdoutcpy, STDOUT_FILENO);
-		// 	close(fd);
-		// }
 		else if (ft_strncmp(command_struct->word_matrix[index], "<<\0", 3) == 0)
 			printf("<< found \n");
 		else if (ft_strncmp(command_struct->word_matrix[index], "<\0", 2) == 0)
