57552d8eb83675a277c19a8a8f722befc6360b32
Guido Meoli <gmeoli@e4r2p3.42roma.it>
2022-10-18 14:48:04 +0200

    OK

--- ft_redirections.c
+++ ft_redirections.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_redirections.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gmeoli <gmeoli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:45:04 by masebast          #+#    #+#             */
-/*   Updated: 2022/10/17 19:28:28 by masebast         ###   ########.fr       */
+/*   Updated: 2022/10/18 14:18:41 by gmeoli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,40 +19,39 @@
 // 	stdincpy = dup(0);
 // }
 
-void	ft_decrease_word_matrix(char **word_matrix)
+char	**ft_decrease_word_matrix(char **word_matrix)
 {
 	int		index;
-	int		temp_index;
 	char	**temp_matrix;
 
 	index = 0;
-	temp_index = 0;
 	while (word_matrix[index])
 		index++;
 	temp_matrix = malloc(sizeof(char *) * index);
-	temp_matrix[index] = NULL;
 	index = 0;
 	while (ft_strncmp(word_matrix[index], ">>\0", 3) != 0 &&
 			ft_strncmp(word_matrix[index], ">\0", 2) != 0 &&
 			word_matrix[index])
 	{
-		temp_matrix[temp_index] = ft_strdup(word_matrix[index]);
+		temp_matrix[index] = ft_strdup(word_matrix[index]);
 		index++;
-		temp_index++;
 	}
-	temp_index = 0;
-	while (temp_matrix[temp_index])
-		temp_index++;
+	temp_matrix[index] = NULL;
+	// index = 0;
+	// while (temp_matrix[index])
+	// 	index++;
+	// ft_free_matrix(word_matrix);
+	// word_matrix = malloc(sizeof(char *) * index + 1);
+	// index = 0;
+	// while (temp_matrix[index])
+	// {
+	// 	word_matrix[index] = ft_strdup(temp_matrix[index]);
+	// 	index++;
+	// }
+	// word_matrix[index] = NULL;
+	// ft_free_matrix(temp_matrix);
 	ft_free_matrix(word_matrix);
-	word_matrix = malloc(sizeof(char *) * temp_index + 1);
-	word_matrix[temp_index] = NULL;
-	temp_index = 0;
-	while (temp_matrix[temp_index])
-	{
-		word_matrix[temp_index] = ft_strdup(temp_matrix[temp_index]);
-		temp_index++;
-	}
-	ft_free_matrix(temp_matrix);
+	return(temp_matrix);
 }
 
 void	ft_redirect(t_command *command_struct, int pipe_index, char **envp)
@@ -71,12 +70,14 @@ void	ft_redirect(t_command *command_struct, int pipe_index, char **envp)
 			if (ft_strncmp(command_struct->word_matrix[index], ">>\0", 3) == 0)
 			{
 				fd = open(command_struct->word_matrix[index + 1], O_APPEND|O_CREAT|O_WRONLY, 0644);
-				ft_decrease_word_matrix(command_struct->word_matrix);
+				// ft_decrease_word_matrix(command_struct->word_matrix);
+				// index++;
 			}
 			else if (ft_strncmp(command_struct->word_matrix[index], ">\0", 2) == 0)
 			{
 				fd = open(command_struct->word_matrix[index + 1], O_TRUNC|O_CREAT|O_WRONLY, 0644);
-				ft_decrease_word_matrix(command_struct->word_matrix);
+				command_struct->word_matrix = ft_decrease_word_matrix(command_struct->word_matrix);
+				index++;
 			}
 			close(STDOUT_FILENO);
 			dup2(fd, STDOUT_FILENO);
